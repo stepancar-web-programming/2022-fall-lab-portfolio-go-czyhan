18770db9be950f965e9e69277e93df4aeba8fbcf
GG <zjjhgzh@gmail.com>
2022-10-10 11:05:56 +0300

    Revert "Revert“
    This reverts commit 664ca29b53a1a96c90c133ea49cc316c8c7db2ff.

--- src/request/api.ts
+++
@@ -1,9 +0,0 @@
-import service from "."
-
-
-export  function getReadme(url:string) {
-    return service({
-        url: url,
-        method: "Get"
-    })
-}
--- src/request/index.ts
+++
@@ -1,3 +0,0 @@
-import axios from "axios";
-const service = axios.create()
-export default service
