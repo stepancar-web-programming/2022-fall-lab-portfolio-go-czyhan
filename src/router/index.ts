0d026517498647da142d65a5836d3cef433a094b
GG <zjjhgzh@gmail.com>
2022-11-06 07:16:42 +0300

    lint test2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
-const router = createRouter({
+var router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes: [
     {
